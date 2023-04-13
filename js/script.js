f77b0450440a1e9a9a75a800fff4548de6161568
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-13 09:41:03 +0200

    add image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      message:'Ciao Vue!'
+      message:'Ciao Vue!',
+      image:{
+        src: 'img/02.webp',
+        alt: 'img'
+      }
     }
-  }
+  },
+
+
+
 }).mount('#app')
